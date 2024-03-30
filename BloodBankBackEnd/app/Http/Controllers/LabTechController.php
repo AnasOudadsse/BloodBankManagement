a41d018f76f1cbc81954aeec00590d3a212257b5
Anas Oudadsse <anas.oudadsse1@gmail.com>
2024-03-30 02:09:02 +0000

    "Added a 'store' method to LabTech controller to create a new lab technician with validation"
    This commit adds a new method, 'store', to the LabTech controller in the BloodBankBackEnd application. The method is used to create a new lab technician in the system with the given validation rules. The validation rules ensure that the provided Cin, PhoneNumber, and Email are unique in the lab_teches table, and that all required fields are provided. The new lab technician is then created using the validated data and the LabTech model's 'create' method. A success message is returned upon successful creation.

--- BloodBankBackEnd/app/Http/Controllers/LabTechController.php
+++ BloodBankBackEnd/app/Http/Controllers/LabTechController.php
@@ -2,8 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\LabTech;
 use Illuminate\Http\Request;
 
 class LabTechController extends Controller
 {
+
+
+    public function store(request $request){
+        $validateLabTech= $request->validate([
+            'Cin' => 'required|unique:lab_teches',
+            'Name' => 'required',
+            'PhoneNumber'=> 'required|unique:lab_teches',
+            'Email'=> 'required|unique:lab_teches', 
+            'BirthDate'=> 'required',
+            'Gender'=> 'required',
+            'EncryptedPassword'=> 'required',
+            'Role' => 'required|in:LabTech',
+            'blood_bank_id' => 'required|in:1'
+        ]);
+
+        LabTech::create($validateLabTech);
+        return response()->json('The labtech has been created successfully');
+    }
+
 }
