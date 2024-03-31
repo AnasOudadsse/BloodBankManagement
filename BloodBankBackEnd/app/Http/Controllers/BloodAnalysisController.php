8254ca0e68de112e66582e394fd5a3be8aa6cd60
Anas Oudadsse <anas.oudadsse1@gmail.com>
2024-03-31 23:55:51 +0000

    "Added functionality to store analysis report file in storage and associate it with a new AnalysisReport model. Logged whether a file was received or not."

--- BloodBankBackEnd/app/Http/Controllers/BloodAnalysisController.php
+++ BloodBankBackEnd/app/Http/Controllers/BloodAnalysisController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\AnalysisReport;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
 
 class BloodAnalysisController extends Controller
 {
@@ -13,8 +16,23 @@ class BloodAnalysisController extends Controller
             'AnalysisReport' => 'required|file|mimes:pdf,docx',
             ]);
 
-
+        if ($request->hasFile('AnalysisReport')){
+            Log::info("file received");
+            $reportPath = $request->file('AnalysisReport')->store('donors/reports','public');
+        }
+        else{
+            Log::info('file not received');
+        }
         
+        $analysisReport = new AnalysisReport([
+            'isGood'  => $validatedAnalysis['isGood'],
+            'AnalysisReport' => $reportPath
+        ]);
+
+        $analysisReport->save();
+
+        return response()->json($analysisReport, 201);
+
     }
 
 }
